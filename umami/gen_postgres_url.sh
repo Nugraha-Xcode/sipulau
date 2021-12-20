c937ed151bf8779500a6193584656bc020ceb710
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2021-12-20 10:07:06 +0700

    Build umami from source

--- umami/gen_postgres_url.sh
+++ umami/gen_postgres_url.sh
@@ -3,5 +3,4 @@
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD//\'/\\\'}
 POSTGRES_PASSWORD=$(node -e "console.log(encodeURIComponent('$POSTGRES_PASSWORD'));")
 echo "DATABASE_URL=postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" > .env.local
-echo "BASE_PATH=$BASE_PATH" >> .env.local
 echo "HASH_SALT=$HASH_SALT" >> .env.local
