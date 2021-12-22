2c27b2e6f499405a1b8691d5315e76b3f8c5a46a
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2021-12-22 16:44:15 +0700

    Try to make mail transporter directly and use proxy directus

--- directus/extensions/src/hooks/send-mail-on-update/index.ts
+++ directus/extensions/src/hooks/send-mail-on-update/index.ts
@@ -1,4 +1,5 @@
 import { defineHook } from "@directus/extensions-sdk";
+import nodemailer from "nodemailer";
 
 type TUserDataEmail = {
   email: string;
@@ -9,8 +10,8 @@ type TInput = {
   status?: string;
 };
 
-export default defineHook(({ filter }, { services, exceptions }) => {
-  const { MailService } = services;
+export default defineHook(({ filter }, { /* services,  */exceptions }) => {
+  // const { MailService } = services;
   const { ServiceUnavailableException, InvalidPayloadException } = exceptions;
 
   filter(
@@ -19,14 +20,24 @@ export default defineHook(({ filter }, { services, exceptions }) => {
       if (!input.jawaban) {
         throw new InvalidPayloadException("Jawaban harus memiliki isi");
       }
-      const mailService = new MailService({ schema });
+      // const mailService = new MailService({ schema });
+      const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_SMTP_HOST as string,
+        port: parseInt(process.env.EMAIL_SMTP_PORT as string),
+        auth: {
+          user: process.env.EMAIL_SMTP_USER,
+          pass: process.env.EMAIL_SMTP_PASSWORD,
+        },
+      });
+      transport.setupProxy("http://192.168.1.28:3128")
       try {
         let userData = await database<TUserDataEmail>("feedback")
           .select("email")
           .where("feedback_id", keys[0])
           .first();
-        if (!userData) throw new Error("Somehow user data doesn't exist")
-        await mailService.send({
+        if (!userData) throw new Error("Somehow user data doesn't exist");
+        // await mailService.send({
+        await transport.sendMail({
           from: `SIPULAU BIG <${process.env.EMAIL_FROM}>`,
           to: userData.email,
           subject: "Feedback SIPULAU",
