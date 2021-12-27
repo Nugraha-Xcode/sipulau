46f7a5aed8f0ee0163690cde9757f33539b4c262
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2021-12-27 13:01:51 +0700

    Revert "Try to make mail transporter directly and use proxy directus"
    This reverts commit 2c27b2e6f499405a1b8691d5315e76b3f8c5a46a.

--- directus/extensions/src/hooks/send-mail-on-update/index.ts
+++ directus/extensions/src/hooks/send-mail-on-update/index.ts
@@ -1,6 +1,4 @@
 import { defineHook } from "@directus/extensions-sdk";
-import nodemailer from "nodemailer";
-import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 type TUserDataEmail = {
   email: string;
@@ -11,12 +9,8 @@ type TInput = {
   status?: string;
 };
 
-interface IMySMTPTransportOptions extends SMTPTransport.Options {
-  proxy: string | undefined;
-}
-
 export default defineHook(({ filter }, { services, exceptions }) => {
-  // const { MailService } = services;
+  const { MailService } = services;
   const { ServiceUnavailableException, InvalidPayloadException } = exceptions;
 
   filter(
@@ -25,25 +19,14 @@ export default defineHook(({ filter }, { services, exceptions }) => {
       if (!input.jawaban) {
         throw new InvalidPayloadException("Jawaban harus memiliki isi");
       }
-      // const mailService = new MailService({ schema });
-      const smtpOptions: IMySMTPTransportOptions = {
-        host: process.env.EMAIL_SMTP_HOST as string,
-        port: parseInt(process.env.EMAIL_SMTP_PORT as string),
-        auth: {
-          user: process.env.EMAIL_SMTP_USER,
-          pass: process.env.EMAIL_SMTP_PASSWORD,
-        },
-        proxy: "http://192.168.1.28:3128",
-      };
-      const transport = nodemailer.createTransport(smtpOptions);
+      const mailService = new MailService({ schema });
       try {
         let userData = await database<TUserDataEmail>("feedback")
           .select("email")
           .where("feedback_id", keys[0])
           .first();
-        if (!userData) throw new Error("Somehow user data doesn't exist");
-        // await mailService.send({
-        await transport.sendMail({
+        if (!userData) throw new Error("Somehow user data doesn't exist")
+        await mailService.send({
           from: `SIPULAU BIG <${process.env.EMAIL_FROM}>`,
           to: userData.email,
           subject: "Feedback SIPULAU",
