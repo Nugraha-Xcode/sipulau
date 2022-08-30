17ddbc2985ef04f46ea66d0200f0a403502d604e
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2022-08-30 11:48:53 +0700

    Fix get directus user id for development

--- next/utils/api/getDirectusUserId.js
+++ next/utils/api/getDirectusUserId.js
@@ -1,43 +1,83 @@
-import http from "http"
+import http from "http";
+import https from "https";
+
+const directusUrl = new URL(process.env.NEXT_PUBLIC_DIRECTUS_URL);
 
 export default function getDirectusUserId(token) {
   return new Promise((resolve, reject) => {
-    const req = http.request(
-      {
-        hostname: "directus",
-        port: 8055,
-        path: "/get-user-id",
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      },
-      (res) => {
-        let data = "";
-        res.on("data", (chunk) => {
-          data += chunk;
-        });
-        res.on("end", () => {
-          if (res.statusCode === 401) {
-            reject({
-              statusCode: res.statusCode,
-              message: "Harap login terlebih dahulu",
-            });
-          } else if (res.statusCode === 200) {
-            let parsedRes = JSON.parse(data);
-            resolve(parsedRes);
-          } else {
-            reject({
-              statusCode: res.statusCode,
-              message: "Terjadi kesalahan pada server",
-            });
-          }
-        });
-        res.on("error", (error) => {
-          reject(error);
-        });
-      }
-    );
+    const req =
+      process.env.NODE_ENV === "production"
+        ? http.request(
+            {
+              hostname: "directus",
+              port: 8055,
+              path: "/get-user-id",
+              method: "GET",
+              headers: {
+                Authorization: "Bearer " + token,
+              },
+            },
+            (res) => {
+              let data = "";
+              res.on("data", (chunk) => {
+                data += chunk;
+              });
+              res.on("end", () => {
+                if (res.statusCode === 401) {
+                  reject({
+                    statusCode: res.statusCode,
+                    message: "Harap login terlebih dahulu",
+                  });
+                } else if (res.statusCode === 200) {
+                  let parsedRes = JSON.parse(data);
+                  resolve(parsedRes);
+                } else {
+                  reject({
+                    statusCode: res.statusCode,
+                    message: "Terjadi kesalahan pada server",
+                  });
+                }
+              });
+              res.on("error", (error) => {
+                reject(error);
+              });
+            }
+          )
+        : https.request(
+            {
+              hostname: directusUrl.hostname,
+              path: directusUrl.pathname + "/get-user-id",
+              method: "GET",
+              headers: {
+                Authorization: "Bearer " + token,
+              },
+            },
+            (res) => {
+              let data = "";
+              res.on("data", (chunk) => {
+                data += chunk;
+              });
+              res.on("end", () => {
+                if (res.statusCode === 401) {
+                  reject({
+                    statusCode: res.statusCode,
+                    message: "Harap login terlebih dahulu",
+                  });
+                } else if (res.statusCode === 200) {
+                  let parsedRes = JSON.parse(data);
+                  resolve(parsedRes);
+                } else {
+                  reject({
+                    statusCode: res.statusCode,
+                    message: "Terjadi kesalahan pada server",
+                  });
+                }
+              });
+              res.on("error", (error) => {
+                reject(error);
+              });
+            }
+          );
 
     req.on("error", (error) => {
       reject(error);
