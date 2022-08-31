3ab7f291256e59a1f5c2ccd849c0f8145831a7b0
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2022-08-31 11:49:22 +0700

    map: handle layer opacity controller

--- next/components/map/layerManagement/OpacityController.js
+++ next/components/map/layerManagement/OpacityController.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useContext, useState, useRef, useEffect } from "react";
+import MapContext from "../../../context/MapContext";
 
-const OpacityController = ({ handleDelete }) => {
+const OpacityController = ({ handleDelete, item }) => {
+  const { map } = useContext(MapContext);
+  const sliderRef = useRef(null);
+  const [layerOpacity, setLayerOpacity] = useState(
+    map.getLayer(item.judul + item.nama)
+      ? map.getPaintProperty(item.judul + item.nama, "raster-opacity") * 100
+      : 100
+  );
+
+  useEffect(() => {
+    sliderRef.current.addEventListener("input", (e) => {
+      map.setPaintProperty(
+        item.judul + item.nama,
+        "raster-opacity",
+        parseInt(e.target.value, 10) / 100
+      );
+    });
+  }, [map, sliderRef.current, item]);
+
   return (
     <div className='relative w-full h-[fit-content] flex flex-col justify-center gap-2 bg-[#E7F5FE] px-5 py-3 rounded-[8px]'>
       {/* <div className='flex justify-between w-full h-[fit-content] font-semibold text-base text-[#232221]'>
@@ -40,8 +59,18 @@ const OpacityController = ({ handleDelete }) => {
 
       <div className='w-full flex gap-2 justify-between items-center'>
         <img src='/images/ic-transparency.svg' alt='icon' />
-        <input type='range' id='vol' name='vol' min='0' max='100' />
-        <p className='text-gray-500 text-xs'>80%</p>
+        <input
+          ref={sliderRef}
+          className='flex-1'
+          type='range'
+          id='vol'
+          name='vol'
+          min='0'
+          max='100'
+          value={layerOpacity}
+          onChange={(e) => setLayerOpacity(e.target.value)}
+        />
+        <p className='text-gray-500 text-xs w-5'>{layerOpacity}</p>
       </div>
     </div>
   );
