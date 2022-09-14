e209457db47209d22ff5f79566acfd5859730307
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2022-09-14 15:57:17 +0700

    Add additional layer API

---
+++ next/pages/api/additional-layers.js
@@ -0,0 +1,27 @@
+import { sipulauPool } from "../../db";
+
+export default async function additionalLayersHandler(req, res) {
+  const { method } = req;
+  if (method !== "GET") {
+    return res
+      .setHeader("Allow", ["GET"])
+      .status(405)
+      .json({ message: `Method ${method} Not Allowed` });
+  }
+
+  let queryResult;
+  try {
+    queryResult = await sipulauPool.query(
+      `
+      SELECT al.layer_id, layer_name, json_agg(json_build_object('layer_type', layer_type, 'url', url, 'min_zoom', min_zoom, 'max_zoom', max_zoom)) layer_defs
+      FROM additional_layers al
+      INNER JOIN additional_layer_def ald ON ald.layer_id = al.layer_id
+      GROUP BY al.layer_id
+      `
+    );
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Terjadi kesalahan pada server" });
+  }
+  return res.json(queryResult.rows);
+}
