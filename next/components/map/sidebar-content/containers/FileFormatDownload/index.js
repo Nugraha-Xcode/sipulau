23e364ff44ceb3682d3df00439bb360448c60836
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2022-09-21 11:52:51 +0700

    upload feature: add nda modal confirmation

--- next/components/map/sidebar-content/containers/FileFormatDownload/index.js
+++ next/components/map/sidebar-content/containers/FileFormatDownload/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../../../../modal";
 import { Button, Dropdown } from "../../core";
 import style from "./FileFormatDownload.module.css";
 
@@ -13,6 +14,8 @@ const FileFormatDownload = ({
     { label: "CSV", value: "csv" },
     { label: "SHP", value: "shp" },
   ];
+  const [ndaModal, setNdaModal] = useState(false);
+  const [isAccept, setAccept] = useState(false);
 
   return (
     <div className={style.container}>
@@ -26,12 +29,74 @@ const FileFormatDownload = ({
       <button
         data-cy='download-feature-add-aoi-button'
         onClick={() => {
-          type.value === "csv" ? handleDownloadCsv() : handleDownloadShp();
+          setNdaModal(true);
         }}
         className={`p-1 bg-main-blue text-white w-full text-sm rounded-lg py-2`}
       >
         <p>Proceed</p>
       </button>
+
+      <Modal
+        isShowing={ndaModal}
+        handleModal={() => {
+          setNdaModal(false);
+        }}
+        size='md'
+      >
+        <div className='py-5 px-8 flex flex-col gap-2'>
+          <div className='flex items-center justify-between'>
+            <img src='/images/big-logo.svg' alt='logo' className='w-14 h-14' />
+            <button
+              onClick={() => {
+                setNdaModal(false);
+              }}
+            >
+              <img
+                src='/images/ic-close.svg'
+                alt='close button'
+                className='w-3 h-3'
+              />
+            </button>
+          </div>
+          <p className='text-xl text-gray-900 font-semibold'>
+            Non-Disclosure Agreement (NDA)
+          </p>
+          <p className='text-xs text-gray-900'>
+            Informasi Geospasial Dasar (IGD) yang terdapat dalam portal ini
+            adalah produk Badan Informasi Geospasial (BIG) dan hak ciptanya
+            dimiliki oleh BIG. Pengguna diijinkan dan dibebaskan untuk
+            mengunduh, mendistribusikan, mengadaptasi atau membuat turunan IGD
+            yang ada dalam website ini, dengan syarat mencantumkan sumber
+            informasi/data berasal dari BIG. Pengguna tidak diperkenankan untuk
+            memperjualbelikan kembali segala data yang diperoleh dari portal
+            ini.
+          </p>
+          <div className='flex items-center gap-2 justify-center my-2'>
+            <input
+              type='checkbox'
+              id='acceptTermCondition'
+              name='acceptTermCondition'
+              onChange={() => {
+                setAccept((prev) => !prev);
+              }}
+            />
+            <label for='acceptTermCondition' className='text-blue-500 text-xs'>
+              Accept Terms & Condition
+            </label>
+          </div>
+          <Button
+            variant='normal'
+            isActive={isAccept}
+            disabled={!isAccept}
+            onClick={() => {
+              type.value === "csv" ? handleDownloadCsv() : handleDownloadShp();
+            }}
+            className='text-sm'
+          >
+            Upload
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 };
