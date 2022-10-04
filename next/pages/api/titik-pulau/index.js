969cfd5236d23ee7992d396b3dd1dcd7be891379
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2022-10-04 10:00:55 +0700

    api: Add advanced query parser

--- next/pages/api/titik-pulau/index.js
+++ next/pages/api/titik-pulau/index.js
@@ -1,192 +1,182 @@
 import { sipulauPool } from "../../../db";
 import getCurrentActiveTable from "../../../utils/api/getCurrentActiveTable";
+import isValidMultiPolygonGeom from "../../../utils/api/isValidMultiPolygonGeom";
+import {
+  parseQuery,
+  QueryError,
+  validColumns,
+} from "../../../utils/api/parseQuery";
 
-const validColumns = [
-  "fid",
-  "id_toponim",
-  "nammap",
-  "alias",
-  "artinam",
-  "sjhnam",
-  "aslbhs",
-  "id_wilayah",
-  "wadmkd",
-  "wadmkc",
-  "wadmkk",
-  "wadmpr",
-  "status",
-  "remark",
-  "pjg_gp",
-  "luas",
-];
+const allowedMethods = ["GET", "POST"];
 
 export default async function tableHandler(req, res) {
   const { method } = req;
-  if (method !== "GET") {
+  if (!allowedMethods.includes(method)) {
     return res
-      .setHeader("Allow", ["GET"])
+      .setHeader("Allow", allowedMethods)
       .status(405)
       .json({ message: `Method ${method} Not Allowed` });
   }
 
-  let { page, ordby: ordBy, orddir: ordDir } = req.query;
+  if (req.method === "GET") {
+    let { page, ordby: ordBy, orddir: ordDir } = req.query;
 
-  // validate page
-  if (page) {
-    page = parseInt(page);
-    if (isNaN(page) || page < 0) {
-      return res.status(400).json({ message: "Halaman tidak valid" });
-    }
-  } else {
-    page = 1;
-  }
-
-  // set limit and offset
-  let limit = 20;
-  let offset = (page - 1) * limit;
-
-  // validate order column
-  if (ordBy) {
-    ordBy = validColumns.includes(ordBy) ? ordBy : "fid";
-  } else {
-    ordBy = "fid";
-  }
-
-  // validate order direction
-  if (ordDir) {
-    ordDir = ordDir.toUpperCase() === "DESC" ? "DESC" : "ASC";
-  } else {
-    ordDir = "ASC";
-  }
-
-  // validate filters
-  let {
-    fid,
-    id_toponim,
-    nammap,
-    artinam,
-    sjhnam,
-    aslbhs,
-    id_wilayah,
-    wadmkd,
-    wadmkc,
-    wadmkk,
-    wadmpr,
-    status,
-    remark,
-    bbox,
-    point,
-  } = req.query;
-  let filters = [];
-  let paramVal = [limit, offset];
-  let param = 3;
-
-  let equalFilters = [
-    [fid, "fid"],
-    [id_toponim, "id_toponim"],
-    [id_wilayah, "id_wilayah"],
-    [wadmkd, "wadmkd"],
-    [wadmkc, "wadmkc"],
-    [wadmkk, "wadmkk"],
-    [wadmpr, "wadmpr"],
-  ];
-  for (const filter of equalFilters) {
-    if (filter[0] !== undefined) {
-      paramVal.push(filter[0]);
-      filters.push(`${filter[1]} = $${param++}`);
-    }
-  }
-
-  let likeFilters = [
-    [nammap, "nammap"],
-    [artinam, "artinam"],
-    [sjhnam, "sjhnam"],
-    [aslbhs, "aslbhs"],
-    [status, "status"],
-  ];
-  for (const filter of likeFilters) {
-    if (filter[0] !== undefined) {
-      paramVal.push(`%${filter[0]}%`);
-      filters.push(`${filter[1]} ILIKE $${param++}`);
-    }
-  }
-
-  // remark filter
-  if (remark !== undefined) {
-    if (remark === "Berpenduduk" || remark === "Tidak Berpenduduk") {
-      paramVal.push(`^${remark}`);
-      filters.push(`SPLIT_PART(remark, ' - ', 2) ~* $${param++}`);
+    // validate page
+    if (page) {
+      page = parseInt(page);
+      if (isNaN(page) || page < 0) {
+        return res.status(400).json({ message: "Halaman tidak valid" });
+      }
     } else {
+      page = 1;
+    }
+
+    // set limit and offset
+    let limit = 20;
+    let offset = (page - 1) * limit;
+
+    // validate order column
+    if (ordBy) {
+      ordBy = validColumns.includes(ordBy) ? ordBy : "fid";
+    } else {
+      ordBy = "fid";
+    }
+
+    // validate order direction
+    if (ordDir) {
+      ordDir = ordDir.toUpperCase() === "DESC" ? "DESC" : "ASC";
+    } else {
+      ordDir = "ASC";
+    }
+
+    // validate filters
+    let {
+      fid,
+      id_toponim,
+      nammap,
+      artinam,
+      sjhnam,
+      aslbhs,
+      id_wilayah,
+      wadmkd,
+      wadmkc,
+      wadmkk,
+      wadmpr,
+      status,
+      remark,
+      bbox,
+      point,
+    } = req.query;
+    let filters = [];
+    let paramVal = [limit, offset];
+    let param = 3;
+
+    let equalFilters = [
+      [fid, "fid"],
+      [id_toponim, "id_toponim"],
+      [id_wilayah, "id_wilayah"],
+      [wadmkd, "wadmkd"],
+      [wadmkc, "wadmkc"],
+      [wadmkk, "wadmkk"],
+      [wadmpr, "wadmpr"],
+    ];
+    for (const filter of equalFilters) {
+      if (filter[0] !== undefined) {
+        paramVal.push(filter[0]);
+        filters.push(`${filter[1]} = $${param++}`);
+      }
+    }
+
+    let likeFilters = [
+      [nammap, "nammap"],
+      [artinam, "artinam"],
+      [sjhnam, "sjhnam"],
+      [aslbhs, "aslbhs"],
+      [status, "status"],
+    ];
+    for (const filter of likeFilters) {
+      if (filter[0] !== undefined) {
+        paramVal.push(`%${filter[0]}%`);
+        filters.push(`${filter[1]} ILIKE $${param++}`);
+      }
+    }
+
+    // remark filter
+    if (remark !== undefined) {
+      if (remark === "Berpenduduk" || remark === "Tidak Berpenduduk") {
+        paramVal.push(`^${remark}`);
+        filters.push(`SPLIT_PART(remark, ' - ', 2) ~* $${param++}`);
+      } else {
+        filters.push(
+          "SPLIT_PART(remark, ' - ', 2) !~* '^Berpenduduk|^Tidak Berpenduduk'"
+        );
+      }
+    }
+
+    // bbox OR point OR aoi filter
+    if (bbox !== undefined) {
+      bbox = bbox.split(",");
+      if (bbox.length !== 4) {
+        return res
+          .status(400)
+          .json({ message: "Format bbox harus: xmin,ymin,xmax,ymax" });
+      }
+      bbox = bbox.map((el) => parseFloat(el));
+      let [xmin, ymin, xmax, ymax] = bbox;
+      if (
+        isNaN(xmin) ||
+        isNaN(ymin) ||
+        isNaN(xmax) ||
+        isNaN(ymax) ||
+        xmin < -180 ||
+        xmin > 180 ||
+        xmax < -180 ||
+        xmax > 180 ||
+        ymin < -90 ||
+        ymin > 90 ||
+        ymax < -90 ||
+        ymax > 90 ||
+        xmin > xmax ||
+        ymin > ymax
+      ) {
+        return res.status(400).json({ message: "bbox tidak valid" });
+      }
+      paramVal.push(xmin, ymin, xmax, ymax);
       filters.push(
-        "SPLIT_PART(remark, ' - ', 2) !~* '^Berpenduduk|^Tidak Berpenduduk'"
+        `geom && ST_MakeEnvelope($${param++}, $${param++}, $${param++}, $${param++}, 4326)`
+      );
+    } else if (point !== undefined) {
+      point = point.split(",");
+      if (point.length !== 2) {
+        return res.status(400).json({ message: "Format point harus: lon,lat" });
+      }
+      point = point.map((el) => parseFloat(el));
+      let [lon, lat] = point;
+      if (
+        isNaN(lon) ||
+        isNaN(lat) ||
+        lon < -180 ||
+        lon > 180 ||
+        lat < -90 ||
+        lat > 90
+      ) {
+        return res.status(400).json({ message: "point tidak valid" });
+      }
+      paramVal.push(lon, lat);
+      filters.push(
+        `ST_DWithin(ST_SetSRID(ST_MakePoint($${param++}, $${param++}), 4326), geom, 0.05)`
       );
     }
-  }
 
-  // bbox OR point filter
-  if (bbox !== undefined) {
-    bbox = bbox.split(",");
-    if (bbox.length !== 4) {
-      return res
-        .status(400)
-        .json({ message: "Format bbox harus: xmin,ymin,xmax,ymax" });
-    }
-    bbox = bbox.map((el) => parseFloat(el));
-    let [xmin, ymin, xmax, ymax] = bbox;
-    if (
-      isNaN(xmin) ||
-      isNaN(ymin) ||
-      isNaN(xmax) ||
-      isNaN(ymax) ||
-      xmin < -180 ||
-      xmin > 180 ||
-      xmax < -180 ||
-      xmax > 180 ||
-      ymin < -90 ||
-      ymin > 90 ||
-      ymax < -90 ||
-      ymax > 90 ||
-      xmin > xmax ||
-      ymin > ymax
-    ) {
-      return res.status(400).json({ message: "bbox tidak valid" });
-    }
-    paramVal.push(xmin, ymin, xmax, ymax);
-    filters.push(
-      `geom && ST_MakeEnvelope($${param++}, $${param++}, $${param++}, $${param++}, 4326)`
-    );
-  } else if (point !== undefined) {
-    point = point.split(",");
-    if (point.length !== 2) {
-      return res.status(400).json({ message: "Format point harus: lon,lat" });
-    }
-    point = point.map((el) => parseFloat(el));
-    let [lon, lat] = point;
-    if (
-      isNaN(lon) ||
-      isNaN(lat) ||
-      lon < -180 ||
-      lon > 180 ||
-      lat < -90 ||
-      lat > 90
-    ) {
-      return res.status(400).json({ message: "point tidak valid" });
-    }
-    paramVal.push(lon, lat);
-    filters.push(
-      `ST_DWithin(ST_SetSRID(ST_MakePoint($${param++}, $${param++}), 4326), geom, 0.05)`
-    );
-  }
+    // combine filters
+    let combinedFilters =
+      filters.length > 0 ? `WHERE ${filters.join(" AND ")}` : "";
 
-  // combine filters
-  let combinedFilters =
-    filters.length > 0 ? `WHERE ${filters.join(" AND ")}` : "";
-
-  let queryResult;
-  try {
-    let tableName = await getCurrentActiveTable("island");
-    queryResult = await sipulauPool.query(
-      `
+    let queryResult;
+    try {
+      let tableName = await getCurrentActiveTable("island");
+      queryResult = await sipulauPool.query(
+        `
       SELECT
         fid, id_toponim, nammap, alias, artinam, sjhnam, aslbhs, id_wilayah, wadmkd,
         wadmkc, wadmkk, wadmpr, status, remark, ST_X(geom) lon, ST_Y(geom) lat,
@@ -196,11 +186,102 @@ export default async function tableHandler(req, res) {
       ORDER BY ${ordBy} ${ordDir}
       LIMIT $1 OFFSET $2
       `,
-      paramVal
-    );
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Terjadi kesalahan pada server" });
+        paramVal
+      );
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Terjadi kesalahan pada server" });
+    }
+    return res.json(queryResult.rows);
+  } else if (req.method === "POST") {
+    let parsedBody;
+    if (typeof req.body === "string") {
+      try {
+        parsedBody = JSON.parse(req.body);
+      } catch (error) {
+        return res.status(400).json({ message: "Body bukan JSON yang valid" });
+      }
+    } else {
+      parsedBody = req.body;
+    }
+
+    let { page, ordBy, ordDir, query } = parsedBody;
+
+    // validate page
+    if (page) {
+      page = parseInt(page);
+      if (isNaN(page) || page < 0) {
+        return res.status(400).json({ message: "Halaman tidak valid" });
+      }
+    } else {
+      page = 1;
+    }
+
+    // set limit and offset
+    let limit = 20;
+    let offset = (page - 1) * limit;
+
+    // validate order column
+    if (ordBy) {
+      ordBy = validColumns.includes(ordBy) ? ordBy : "fid";
+    } else {
+      ordBy = "fid";
+    }
+
+    // validate order direction
+    if (ordDir) {
+      ordDir = ordDir.toUpperCase() === "DESC" ? "DESC" : "ASC";
+    } else {
+      ordDir = "ASC";
+    }
+
+    // parse query
+    let filterOpts = {
+      filterDirectives: [],
+      filterValues: [limit, offset],
+      filterParam: 3,
+    };
+    if (query) {
+      try {
+        parseQuery(query, filterOpts);
+      } catch (error) {
+        if (error instanceof QueryError) {
+          return res.status(400).json({ message: error.message });
+        } else {
+          console.error(error);
+          return res
+            .status(500)
+            .json({ message: "Terjadi kesalahan pada server" });
+        }
+      }
+    }
+
+    // combine filters
+    let combinedFilters =
+      filterOpts.filterDirectives.length > 0
+        ? `WHERE ${filterOpts.filterDirectives.join(" AND ")}`
+        : "";
+
+    let queryResult;
+    try {
+      let tableName = await getCurrentActiveTable("island");
+      queryResult = await sipulauPool.query(
+        `
+        SELECT
+          fid, id_toponim, nammap, alias, artinam, sjhnam, aslbhs, id_wilayah, wadmkd,
+          wadmkc, wadmkk, wadmpr, status, remark, ST_X(geom) lon, ST_Y(geom) lat,
+          pjg_gp, luas
+        FROM ${tableName}
+        ${combinedFilters}
+        ORDER BY ${ordBy} ${ordDir}
+        LIMIT $1 OFFSET $2
+        `,
+        filterOpts.filterValues
+      );
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Terjadi kesalahan pada server" });
+    }
+    return res.json(queryResult.rows);
   }
-  return res.json(queryResult.rows);
 }
---
+++ next/utils/api/parseQuery.js
@@ -0,0 +1,268 @@
+import isValidMultiPolygonGeom from "./isValidMultiPolygonGeom";
+
+export class QueryError extends Error {}
+
+export const validColumns = [
+  "fid",
+  "id_toponim",
+  "nammap",
+  "alias",
+  "artinam",
+  "sjhnam",
+  "aslbhs",
+  "id_wilayah",
+  "wadmkd",
+  "wadmkc",
+  "wadmkk",
+  "wadmpr",
+  "status",
+  "remark",
+  "pjg_gp",
+  "luas",
+];
+
+const generateParamArr = (value, filterOpts, operator, columnName) => {
+  if (!Array.isArray(value) || value.length === 0) {
+    throw new QueryError(
+      `${operator} operator must contains array of values. Location: ${columnName}`
+    );
+  }
+  const paramArr = [];
+  let newParam = filterOpts.filterParam;
+  for (const el of value) {
+    paramArr.push(`$${newParam++}`);
+    filterOpts.filterValues.push(el);
+  }
+  // update parent filterOpts
+  filterOpts.filterParam = newParam;
+  return paramArr;
+};
+
+const columnParser = (queryObj, columnName, filterOpts) => {
+  const queryObjKeys = Object.keys(queryObj);
+  if (
+    typeof queryObj !== "object" ||
+    Array.isArray(queryObj) ||
+    queryObjKeys.length !== 1
+  ) {
+    throw new QueryError(
+      "Column query must be object with single operator key (_eq, _lte, etc.)"
+    );
+  }
+  const operator = queryObjKeys[0];
+  const value = queryObj[operator];
+
+  // array values
+  if (operator === "_in" || operator === "_nin") {
+    let directive = "";
+
+    switch (operator) {
+      case "_in":
+        directive = "IN";
+        break;
+
+      case "_nin":
+        directive = "NOT IN";
+        break;
+
+      default:
+        throw new Error("How do we get here?");
+    }
+    // push filter directive
+    filterOpts.filterDirectives.push(
+      `${columnName} ${directive} (${generateParamArr(
+        value,
+        filterOpts,
+        operator,
+        columnName
+      )})`
+    );
+  } else {
+    let newParam = filterOpts.filterParam;
+    switch (operator) {
+      case "_eq":
+        filterOpts.filterValues.push(value);
+        filterOpts.filterDirectives.push(`${columnName} = $${newParam++}`);
+        break;
+
+      case "_neq":
+        filterOpts.filterValues.push(value);
+        filterOpts.filterDirectives.push(`${columnName} != $${newParam++}`);
+        break;
+
+      case "_lt":
+        filterOpts.filterValues.push(value);
+        filterOpts.filterDirectives.push(`${columnName} < $${newParam++}`);
+        break;
+
+      case "_lte":
+        filterOpts.filterValues.push(value);
+        filterOpts.filterDirectives.push(`${columnName} <= $${newParam++}`);
+        break;
+
+      case "_gt":
+        filterOpts.filterValues.push(value);
+        filterOpts.filterDirectives.push(`${columnName} > $${newParam++}`);
+        break;
+
+      case "_gte":
+        filterOpts.filterValues.push(value);
+        filterOpts.filterDirectives.push(`${columnName} >= $${newParam++}`);
+        break;
+
+      case "_null":
+        filterOpts.filterDirectives.push(`${columnName} IS NULL`);
+        break;
+
+      case "_nnull":
+        filterOpts.filterDirectives.push(`${columnName} IS NOT NULL`);
+        break;
+
+      case "_contains":
+        filterOpts.filterValues.push(`%${value}%`);
+        filterOpts.filterDirectives.push(`${columnName} ILIKE $${newParam++}`);
+        break;
+
+      case "_ncontain":
+        filterOpts.filterValues.push(`%${value}%`);
+        filterOpts.filterDirectives.push(
+          `${columnName} NOT ILIKE $${newParam++}`
+        );
+        break;
+
+      default:
+        throw new QueryError(`Invalid operator ${operator} in ${columnName}`);
+    }
+    // update parent filterOpts
+    filterOpts.filterParam = newParam;
+  }
+};
+
+const geomColumnParser = (queryObj, columnName, filterOpts) => {
+  const queryObjKeys = Object.keys(queryObj);
+  if (
+    typeof queryObj !== "object" ||
+    Array.isArray(queryObj) ||
+    queryObjKeys.length !== 1
+  ) {
+    throw new QueryError(
+      "Column query must be object with single operator key (e.g. _within)"
+    );
+  }
+  const operator = queryObjKeys[0];
+  const value = queryObj[operator];
+
+  let newParam = filterOpts.filterParam;
+  switch (operator) {
+    case "_within":
+      if (isValidMultiPolygonGeom(value)) {
+        filterOpts.filterValues.push(value);
+        filterOpts.filterDirectives.push(
+          `ST_Within(geom, ST_GeomFromGeoJSON($${newParam++}))`
+        );
+      } else {
+        throw new QueryError(
+          `_within operator must provide a valid MultiPolygon GeoJSON`
+        );
+      }
+      break;
+
+    default:
+      throw new QueryError(`Invalid operator ${operator} in ${columnName}`);
+  }
+  // update parent filterOpts
+  filterOpts.filterParam = newParam;
+};
+
+const logicalOperatorParser = (logicalOperator, queryObj, filterOpts) => {
+  if (!Array.isArray(queryObj) || queryObj.length === 0) {
+    throw new QueryError(
+      `_and _or must contains non empty array of query object`
+    );
+  }
+  let logicalOpSql = "";
+  switch (logicalOperator) {
+    case "_and":
+      logicalOpSql = " AND ";
+      break;
+
+    case "_or":
+      logicalOpSql = " OR ";
+      break;
+
+    default:
+      throw new QueryError("Invalid logical operator");
+  }
+  // generate filterOpts for group
+  const groupFilterOpts = { ...filterOpts, filterDirectives: [] };
+
+  for (const child of queryObj) {
+    const queryObjKeys = Object.keys(child);
+    if (
+      typeof child !== "object" ||
+      Array.isArray(child) ||
+      queryObjKeys.length !== 1
+    ) {
+      throw new QueryError(
+        "query must be object with single key (_and/_or/{column_name})"
+      );
+    }
+    if (queryObjKeys[0] === "_and" || queryObjKeys[0] === "_or") {
+      logicalOperatorParser(
+        queryObjKeys[0],
+        child[queryObjKeys[0]],
+        groupFilterOpts
+      );
+    } else if (queryObjKeys[0] === "geom") {
+      geomColumnParser(
+        child[queryObjKeys[0]],
+        queryObjKeys[0],
+        groupFilterOpts
+      );
+    } else if (validColumns.includes(queryObjKeys[0])) {
+      columnParser(child[queryObjKeys[0]], queryObjKeys[0], groupFilterOpts);
+    } else {
+      throw new QueryError(`Invalid column name: ${queryObjKeys[0]}`);
+    }
+    // update parent filterOpts
+    filterOpts.filterValues = groupFilterOpts.filterValues;
+    filterOpts.filterParam = groupFilterOpts.filterParam;
+  }
+
+  // push filter directive
+  filterOpts.filterDirectives.push(
+    `(${groupFilterOpts.filterDirectives.join(logicalOpSql)})`
+  );
+};
+
+export const parseQuery = (queryObj, filterOpts) => {
+  if (!filterOpts) {
+    filterOpts = { filterDirectives: [], filterValues: [], filterParam: 1 };
+  }
+
+  const queryObjKeys = Object.keys(queryObj);
+  if (
+    typeof queryObj !== "object" ||
+    Array.isArray(queryObj) ||
+    queryObjKeys.length !== 1
+  ) {
+    throw new QueryError(
+      "query must be object with single key (_and/_or/{column_name})"
+    );
+  }
+
+  if (queryObjKeys[0] === "_and" || queryObjKeys[0] === "_or") {
+    logicalOperatorParser(
+      queryObjKeys[0],
+      queryObj[queryObjKeys[0]],
+      filterOpts
+    );
+  } else if (queryObjKeys[0] === "geom") {
+    geomColumnParser(queryObj[queryObjKeys[0]], queryObjKeys[0], filterOpts);
+  } else if (validColumns.includes(queryObjKeys[0])) {
+    columnParser(queryObj[queryObjKeys[0]], queryObjKeys[0], filterOpts);
+  } else {
+    throw new QueryError(`Invalid column name: ${queryObjKeys[0]}`);
+  }
+  return filterOpts;
+};
