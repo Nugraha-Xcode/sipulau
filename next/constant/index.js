fc0ed26035dad7747befff1e7a7f8c495fab634b
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2022-10-07 13:17:53 +0700

    constant: add operator

--- next/constant/index.js
+++ next/constant/index.js
@@ -7,14 +7,16 @@ export const UploadLayerDatabase = "UploadLayerDatabase";
 export const UploadLayerStore = "UploadLayerStore";
 
 export const filterOperatosList = [
+  { label: "Contains", value: "_contains" },
+  { label: "Doesn't contain", value: "_ncontains" },
   { label: "Equals", value: "_eq" },
   { label: "Doesn't equal", value: "_neq" },
   { label: "Less than", value: "_lt" },
   { label: "Less than or equal to", value: "_lte" },
   { label: "Greater than", value: "_gt" },
   { label: "Greater than or equal to", value: "_gte" },
-  { label: "Contains", value: "_contains" },
-  { label: "Doesn't contain", value: "_ncontains" },
+  { label: "Is null", value: "_null" },
+  { label: "Isn't null", value: "_nnull" },
 ];
 
 export const filterGroupList = [
